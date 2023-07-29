df8667a9d7715d9d05721f1ce20dc0ec12794b1c
gl401387338@gmail.com <gl5644838>
2023-07-29 14:52:15 +0800

    [项目名称]：搭建环境---designmode [ID]：#0 [类型]：feature [改动]：1.增加vscode的基本配置文件。2.搭建designmode的基础环节。3.测试hello代码 [影响面]：无

---
+++ exercise/0hello.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: gl401387338@gmail.com gl5644838
+ * @Date: 2023-07-29 10:51:53
+ * @LastEditors: gl401387338@gmail.com gl5644838
+ * @LastEditTime: 2023-07-29 14:38:11
+ * @FilePath: \designMode\exercise\hello.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+#include <iostream>
+///#include <bits/stdc++.h>
+
+int main()
+{
+    std::cout << "hello world!" << std::endl;
+
+    system("pause");
+    return 0;
+}
